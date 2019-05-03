fb7a32f486e2d4bfba6e1db45a039b70aabcaf80
Jeremy Schiff <jeremyschiff515@gmail.com>
2019-05-03 17:53:18 -0400

    Final model design
    Note that the data is not included due to size

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Conv1D, Dropout, Conv2D, MaxPooling2D
-from keras.optimizers import SGD, Adagrad, Adam
+from keras.layers import Dense, Activation, Flatten, Conv1D, Dropout, Conv2D, MaxPooling2D, BatchNormalization
+from keras.optimizers import SGD, Adagrad, Adam, Nadam
 from keras.utils import np_utils
 from keras import backend
 
 
 from keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger, LambdaCallback
 
-T = 10   # Number of time steps
+T = 100   # Number of time steps
 R = 15   # Number of players
 F = 15   # Number of features
 batch_size = 128
@@ -19,8 +19,8 @@ print("Shape of input should be (N x T x F x 2R) or", (T, F, 2*R))
 
 
 # Load data
-data = np.load("./data.pkl.npy")
-scores = np.load("./outputs.pkl.npy")
+data = np.load("./2003-2019-data.pkl.npy")
+scores = np.load("./2003-2019-outputs.pkl.npy")
 print("data.shape: ", data.shape)
 
 
@@ -28,14 +28,13 @@ print("data.shape: ", data.shape)
 data = np.swapaxes(data, 1, 3)
 data = np.swapaxes(data, 1, 2)
 
-data = np.concatenate((data, data), axis=0)
-scores = np.concatenate((scores, scores), axis=0)
 
 print("swapaxes_data.shape: ", data.shape)
 print("scores.shape: ", scores.shape)
 
 num_filters = R*2
 kernel_size = 5
+drop_out_rate = 0.1
 
 
 ################################################################################################################
@@ -45,27 +44,63 @@ kernel_size = 5
 # # Create neural network 
 model = Sequential()
 
-# Convolution sequence
-# model.add(Conv2D(filters=num_filters, kernel_size=(kernel_size, kernel_size), input_shape=(T, F, 2*R)))
-# # model.add(Activation('relu'))
-# model.add(MaxPooling2D())
-# model.add(Conv2D(filters=num_filters, kernel_size=2))
-# model.add(MaxPooling2D())
+model.add(Conv2D(filters=num_filters, kernel_size=(50,3), padding='same', data_format='channels_last', use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(MaxPooling2D(padding='same'))
 
-# Fully connected layer
+model.add(Conv2D(filters=num_filters, kernel_size=(20,3), padding='same', data_format='channels_last', use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(MaxPooling2D(padding='same'))
+
+
+model.add(Conv2D(filters=num_filters, kernel_size=(10,3), padding='same', data_format='channels_last', use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(MaxPooling2D(padding='same'))
+
+model.add(Conv2D(filters=num_filters, kernel_size=(5,3), padding='same', data_format='channels_last', use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(MaxPooling2D(padding='same'))
+
+model.add(Conv2D(filters=num_filters, kernel_size=(3,3), padding='same', data_format='channels_last', use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(MaxPooling2D(padding='same'))
+
 model.add(Flatten())
-# model.add(Dropout(0.4))
-model.add(Dense(2048, activation='relu'))
-# model.add(Dropout(0.1))
-# model.add(Dense(1024, activation='relu'))
-model.add(Dense(2))
 
+model.add(Dropout(drop_out_rate))
+
+model.add(Dense(2048, use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Dropout(drop_out_rate))
+
+model.add(Dense(1024, use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Dropout(drop_out_rate))
+
+model.add(Dense(512,use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Dropout(drop_out_rate))
+
+model.add(Dense(256,use_bias=False))
+model.add(BatchNormalization())
+model.add(Activation("relu"))
+model.add(Dropout(drop_out_rate))
+
+
+model.add(Dense(2))
 
 ################################################################################################################
 
 # Optimizer
-# sgd = SGD(lr=0.0001, nesterov=True, decay=1e-6, momentum=0.9)
-optimizer = Adagrad(lr=0.0001, decay=0)
+optimizer = Adam(lr=0.001)
 
 
 # User-defined function to compute accuracy of game prediction
@@ -77,11 +112,11 @@ def accuracy(y_true, y_pred):
 
 
 # Compile the computational graph 
-model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=["mae", accuracy])
+model.compile(loss='logcosh', optimizer=optimizer, metrics=["mae", accuracy])
 
 
 # Setting how much of the total data is used for validation
-validation_split = 0.5
+validation_split = 0.1
 n_epochs         = 30
 
 
